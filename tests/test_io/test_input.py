da46e0d8d0f642cb2b592b485a1b3b2c2d7bec53
Oleksandr Semytskyi <oleksandr.semytskyi@gmail.com>
2024-03-16 19:18:10 +0200

    Finish testing first method

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -1,10 +1,25 @@
+import pytest
+
 import app.io.input as input
 
 
 EXPECTED_READ_VALUE_FROM_TXT_FILE = "Hello everyone"
+EXPECTED_READ_VALUE_FROM_CSV_FILE = "TEST First\ntest good\nmark 10"
 TXT_FILE_PATH = "tests/data/input_for_test.txt"
+CSV_FILE_PATH = "tests/data/input_for_test.csv"
+NON_EXISTENT_FILE_PATH = "aaa/daaa.txt"
 
 
 def test_read_text_from_file_simple():
     actual_value = input.read_text_from_file_simple(TXT_FILE_PATH)
     assert EXPECTED_READ_VALUE_FROM_TXT_FILE == actual_value
+
+
+def test_read_text_from_file_simple_raise_exception():
+    with pytest.raises(FileNotFoundError):
+        input.read_text_from_file_simple(NON_EXISTENT_FILE_PATH)
+
+
+def test_read_text_from_file_simple_csv_file():
+    actual_value = input.read_text_from_file_simple(CSV_FILE_PATH)
+    assert EXPECTED_READ_VALUE_FROM_CSV_FILE == actual_value
